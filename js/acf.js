af8ef5a31415259fcb06105d8ef8dddaae4aee40
modemlooper <modemlooper@gmail.com>
2022-03-05 09:45:53 -0800

    edit view

--- js/acf.js
+++ js/acf.js
@@ -20,7 +20,7 @@ acf.add_filter('color_picker_args', function( args, field ){
     const field_name = field[0].getAttribute('data-name');
 
     if ( 'toolbar_color' === field_name ) {
-        if(appp_data !== undefined && appp_data !== null) {
+        if(window.appp_data !== undefined && window.appp_data !== null) {
             const palette = JSON.parse(appp_data.color_palettes);
             const array = Object.values(palette);
             args.palettes = array;
