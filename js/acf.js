47897bcccb7dc57b4bbd9c0ed40c6c7c21c80900
modemlooper <modemlooper@gmail.com>
2022-03-05 09:36:38 -0800

    Update acf.js

--- js/acf.js
+++ js/acf.js
@@ -20,10 +20,11 @@ acf.add_filter('color_picker_args', function( args, field ){
     const field_name = field[0].getAttribute('data-name');
 
     if ( 'toolbar_color' === field_name ) {
-        
-        const palette = JSON.parse(appp_data.color_palettes);
-        const array = Object.values(palette);
-        args.palettes = array;
+        if(appp_data !== undefined && appp_data !== null) {
+            const palette = JSON.parse(appp_data.color_palettes);
+            const array = Object.values(palette);
+            args.palettes = array;
+        }
     }
 
     // return
