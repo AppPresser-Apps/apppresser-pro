1fa9e2ee010e3327ccde283b511d89bfffbc0913
modemlooper <modemlooper@gmail.com>
2023-02-09 06:58:47 -0800

    fix container flex direction

--- blocks/layout/container/block.php
+++ blocks/layout/container/block.php
@@ -195,7 +195,7 @@ $comma = $background_img && 'none' !== $background_gradient['type'] ? ', ' : ' '
 
 	#<?php echo esc_attr( $id ); ?> .block-editor-block-list__layout {
 		display: flex;
-		flex-direction: <?php echo esc_attr( $flex['flex_direction'] ); ?>;
+		flex-direction: <?php echo esc_attr( $flex['flex_direction'] ); ?> !important;
 		justify-content: <?php echo esc_attr( $flex['justify_content'] ); ?>;
 		align-items: <?php echo esc_attr( $flex['align_items'] ); ?>;
 		height: <?php echo $flexheight; ?>;
