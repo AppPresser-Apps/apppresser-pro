ed4766613f46c8c57f13ed7ed33404c186f9c6d8
modemlooper <modemlooper@gmail.com>
2023-05-14 08:22:41 -0700

    remove app dev from plugin files

--- vendors/app-files/www/build/index-c532d7cb.js
+++
@@ -1,7 +0,0 @@
-import { r as registerPlugin } from './index-0f2ea1ed.js';
-
-const Preferences = registerPlugin('Preferences', {
-    web: () => import('./web-519c8d82.js').then(m => new m.PreferencesWeb()),
-});
-
-export { Preferences as P };
